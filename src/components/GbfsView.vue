297818f5ab03deb3c520b46ebc7a3bea789a10dd
Maximilian Richt <maxi@richt.name>
2019-10-03 14:43:20 +0200

    feat(map): use wikimedia maps temporarily

--- src/components/GbfsView.vue
+++ src/components/GbfsView.vue
@@ -20,8 +20,8 @@
       return {
         zoom: this.$appConfig.DEFAULT_ZOOM,
         center: latLng.apply(null, this.$appConfig.DEFAULT_LOCATION),
-        url: "http://{s}.tile.osm.org/{z}/{x}/{y}.png",
-        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+        url: "https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}{r}.png",
+        attribution: '<a href="https://foundation.wikimedia.org/wiki/Maps_Terms_of_Use">Wikimedia Maps</a> | &copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
         gbfsURL: this.$appConfig.GBFS_URL,
       }
     },
