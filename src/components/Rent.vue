fb1ff4a75c44c50c5d622d63227fdcfbb157fdc9
Maximilian Richt <maxi@richt.name>
2020-10-20 23:11:33 +0200

    fix(rent): relayout rent form

--- src/components/Rent.vue
+++ src/components/Rent.vue
@@ -1,11 +1,8 @@
 <template>
   <v-dialog v-model="show" max-width="400">
     <v-card>
-      <v-card-title class="text-h6" primary-title>
-        {{ $t('message.rent.rent')}}
-      </v-card-title>
       <v-form v-model="valid" ref="rentBikeForm" @submit.prevent="startRent">
-        <v-container class="py-0">
+        <v-container class="pt-6 pb-0">
           <v-row v-if="rentError">
             <v-col col="12" md="12">
              <v-alert
@@ -19,7 +16,7 @@
             </v-col>
           </v-row>
           <v-row>
-            <v-col cols="6" md="6">
+            <v-col cols="6" md="6" class="py-0 pr-0">
               <v-text-field
                   type="text"
                   :label="$t('message.rent.bikenumber')"
@@ -32,10 +29,9 @@
                   pattern="[0-9]*"
                 ></v-text-field>
             </v-col>
-            <v-col cols="6" md="6" class="text-center">
+            <v-col cols="6" md="6" class="py-0 text-right">
               <v-btn class="mt-2" color="success" v-bind:disabled="!valid" :loading="loading" @click="startRent">
-                <v-icon>{{ mdi.lockOpenVariant }}</v-icon>&nbsp;
-                <span>{{ $t('message.rent.unlock') }}</span>
+                {{ $t('message.rent.to-rent') }}
               </v-btn>
             </v-col>
           </v-row>
@@ -53,8 +49,7 @@
           <v-list-item-subtitle v-if="rent.bike.lock">{{ $t('message.rent.unlockcode') }}: <span class="rent-unlock-key">{{rent.bike.lock.unlock_key}}</span></v-list-item-subtitle>
           <v-list-item-subtitle>{{ $t('message.rent.renting-for') }} <ticking-time :datetime="rent.rent_start" /></v-list-item-subtitle>
           <v-btn color="success" @click="endRent(rent.id)" v-bind:loading="loadingRents.includes(rent.id)">
-            <v-icon>{{ mdi.lock }}</v-icon>&nbsp;
-            <span>{{ $t('message.rent.finish-rent') }}</span>
+            {{ $t('message.rent.finish-rent') }}
           </v-btn>
         </v-list-item-content>
       </v-list-item>
