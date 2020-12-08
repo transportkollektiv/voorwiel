655f7952dbbd42bebebdbab28c06303203b7dd28
mlange304 <michael.lange@iteratec.com>
2020-12-08 12:05:43 +0100

    Textbox for Error Message

--- src/components/CustomLogin.vue
+++ src/components/CustomLogin.vue
@@ -1,17 +1,16 @@
 
 <template>
-  <v-dialog v-model="show" max-width="400">
+  <v-dialog v-model="show" max-width="500">
     <v-card>
       <v-card-title class="text-h5 grey lighten-2" primary-title>
-        {{ $t('message.login.login-with') }}
+        {{ $t("message.login.login-with") }}
       </v-card-title>
       <v-card-actions>
-
-<v-form v-model="valid" ref="rentBikeForm" @submit.prevent="startRent">
-        <v-container class="pb-0">
-          <v-row class="pt-5">
-            <v-col cols="12" md="6" class="py-0 pr-0">
-              <v-text-field
+        <v-form v-model="valid" ref="rentBikeForm" @submit.prevent="startRent">
+          <v-container class="pb-0">
+            <v-row class="pt-5">
+              <v-col cols="12" md="12" class="py-0 pr-0">
+                <v-text-field
                   type="text"
                   :label="$t('message.login.username')"
                   v-model="username"
@@ -21,13 +20,12 @@
                   inputmode="text"
                   pattern="[0-9a-zA-Z]*"
                 ></v-text-field>
-            </v-col>
-            <v-col cols="1" md="1" class="py-0 text-right">
-            </v-col>
-          </v-row> 
-          <v-row class="pt-5">
-            <v-col cols="12" md="12" class="py-0 pr-0">
-              <v-text-field
+              </v-col>
+              <v-col cols="1" md="1" class="py-0 text-right"> </v-col>
+            </v-row>
+            <v-row class="pt-5">
+              <v-col cols="12" md="12" class="py-0 pr-0">
+                <v-text-field
                   type="password"
                   :label="$t('message.login.password')"
                   v-model="password"
@@ -37,70 +35,80 @@
                   inputmode="text"
                   pattern="[0-9a-zA-Z]*"
                 ></v-text-field>
-            </v-col>
-          </v-row>
+              </v-col>
+            </v-row>
             <v-row class="pt-5">
-            <v-col cols="6" md="6" class="py-0 text-right">
-              <v-btn class="mt-2" color="success" v-bind:disabled="
-              !valid" :loading="loading" @click="authenticate()">
-                {{ "Login" }}
-              </v-btn>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-form>
-
+              <v-col cols="6" md="6" class="py-0 text-right">
+                <v-btn
+                  class="mt-2"
+                  color="success"
+                  v-bind:disabled="!valid"
+                  :loading="loading"
+                  @click="authenticate()"
+                >
+                  {{ "Login" }}
+                </v-btn>
+              </v-col>
+            </v-row>
+          </v-container>
           
+        </v-form>
+         
       </v-card-actions>
     </v-card>
+    <v-container test color: red>
+  <h2 v-if ="wrongPW">Falsches Passwort </h2>
+             </v-container>
   </v-dialog>
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 
-  export default {
-    data() {
-      return {
-        show: true,
-        loading: false,
-        valid: false,
-        username: '',
-        password: ''
+export default {
+  data() {
+    return {
+      wrongPW: false,
+      show: true,
+      loading: false,
+      valid: false,
+      username: "",
+      password: "",
+    };
+  },
+  methods: {
+    authenticate() {
+      let params = new URLSearchParams(location.search);
+      if (params.get("redirect")) {
+        sessionStorage.setItem("redirect", params.get("redirect"));
       }
-    },
-    methods: {
-      authenticate() {        
-        let params = new URLSearchParams(location.search);
-        if (params.get('redirect')) {
-          sessionStorage.setItem('redirect', params.get('redirect'));
-        }
       try {
         // TODO relative path
         let auth_url = "http://localhost:8000/api-token-auth/";
-        
-        axios.post( auth_url, {
-          username : this.username,
-          password : this.password
-        })
-        .then( (response) => {
-          location.href = "/login/return/?token=" + response.data.token 
-        })          
-        .catch( 
-          console.log("Error")
-          // TODO Fehlerbehandlung einfügen
-        )
+
+        axios
+          .post(auth_url, {
+            username: this.username,
+            password: this.password,
+          })
+          .then((response) => {
+            location.href = "/login/return/?token=" + response.data.token;
+          })
+          .catch(
+            console.log("Error im ersten Catch"),
+           this.wrongPW = true
+          );
       } catch (err) {
-        console.err("Error: \n" + err)
+        console.err("Error im zweiten Catch: \n" + err);
       }
-      },
     },
-    watch: {
-      show(current) {
-        if (current === false) {
-          this.$router.push('/');
-        }
+  },
+  watch: {
+    show(current) {
+      if (current === false) {
+        this.$router.push("/");
       }
-    }
-  };
+    },
+  },
+};
 </script>
